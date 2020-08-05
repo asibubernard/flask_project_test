d2dfbd89ef382cf7f654198dcbee30814bfcc0b0
Ebo Asibu <asibubernard@gmail.com>
2020-08-05 12:25:20 +0000

    added a html content to routes.py

--- flask_test/routes.py
+++ flask_test/routes.py
@@ -3,4 +3,13 @@ from flask_test import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Bernard'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Flask_Test_Project</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
