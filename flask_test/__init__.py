214d7b27ea3a51ac1abee27856d9d087bdd0209e
Ebo Asibu <asibubernard@gmail.com>
2020-08-05 17:27:13 +0000

    add login to project

--- flask_test/__init__.py
+++ flask_test/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config# new
 
 app = Flask(__name__)
+app.config.from_object(Config) # new 
 
 from flask_test import routes
 
--- flask_test/routes.py
+++ flask_test/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from flask_test import app 
-
+from flask_test.forms import LoginForm # new
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', email=email, posts=posts)
+
+@app.route('/login') # new
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
