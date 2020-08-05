68d286e10c8a1d7e6725016ab798b771e41b0015
Ebo Asibu <asibubernard@gmail.com>
2020-08-05 18:07:57 +0000

    add validation with flash messages to login

--- flask_test/routes.py
+++ flask_test/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect # new
 from flask_test import app 
-from flask_test.forms import LoginForm # new
+from flask_test.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -18,7 +18,11 @@ def index():
     ]
     return render_template('index.html', title='Home', email=email, posts=posts)
 
-@app.route('/login') # new
+@app.route('/login', methods=['GET', 'POST']) # new
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
