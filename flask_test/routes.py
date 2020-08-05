c950a1c316bffd134102887a79e62074ee7ccfff
Ebo Asibu <asibubernard@gmail.com>
2020-08-05 15:56:42 +0000

    added conditions, loops and template inheritance

--- flask_test/routes.py
+++ flask_test/routes.py
@@ -1,8 +1,19 @@
 from flask import render_template
 from flask_test import app 
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     email = {'emailID': 'asibubernard@email.com'}
-    return render_template('index.html', title='Home', email=email)
+    posts = [
+        {
+            'author': {'username': 'Maxwell'},
+            'body' : 'It is a great day in Ghana'
+        },
+        {
+            'author': {'username': 'Michael'},
+            'body': 'The world is becoming a better place',
+        }
+    ]
+    return render_template('index.html', title='Home', email=email, posts=posts)
